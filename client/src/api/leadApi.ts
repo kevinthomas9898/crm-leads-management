1bdc6d429600277a901053371cc7e36fb8d16d00
Kevin Thomas <kevin@avdevs.com>
2026-05-09 08:54:45 +0530

    Update production API URL

--- client/src/api/leadApi.ts
+++ client/src/api/leadApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://crm-leads-management-server.onrender.com/api",
 });
 
 export const fetchLeads = async (params: any) => {
